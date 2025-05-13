d6e3e3caae8d433680ecd60d36fe296ac6fdca54
7isanumber <sevenbeasley05@gmail.com>
2025-05-13 16:03:47 -0600

    initial setup

---
+++ server.js
@@ -0,0 +1,44 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+
+app.use('/', indexRouter)
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+process.once('SIGUSR2', () => {
+  server.close(() => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server closed from SIGINT');
+    process.exit(0);
+  });
+});
+
+
